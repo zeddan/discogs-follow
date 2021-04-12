94edaa99ade2791a72eec8d51a69dcdbfd5a1f1b
zeddan <robin.saaf@gmail.com>
2021-04-12 23:40:47 +0200

    Add Capistrano

--- Gemfile
+++ Gemfile
@@ -54,6 +54,16 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem "capistrano"
+  gem "capistrano3-puma"
+  gem 'capistrano-bundler'                   # support for bundler
+  gem 'capistrano-rails'                     # automatic migrations and asset compilation
+  gem 'capistrano-rbenv'                     # idiomatic rbenv support
+  gem 'capistrano-rbenv-install'             # ensures the right ruby version is installed
+  gem 'capistrano-postgresql'                # plug-n-play postgresql
+  gem 'capistrano-safe-deploy-to', '~> 1.1'  # ensures deploy path for the app exists
+  gem 'capistrano-ssh-doctor'                # helps with debugging ssh-agent forwarding
+
 end
 
 group :test do
---
+++ config/deploy.rb
@@ -0,0 +1,76 @@
+lock "~> 3.16.0"
+
+server "178.62.34.65", port: 22, roles: %i[web app db], primary: true
+
+set :rbenv_ruby, '2.7.2'
+
+set :repo_url,        "git@github.com:zeddan/discogs-follow.git"
+set :branch,          "main"
+set :application,     "discogs-follow"
+# set :website_url,     "robinsaaf.se/discogs-follow"
+set :user,            "zeddan"
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+# set :enable_ssl,      true
+# set :ssl_certificate, "/etc/letsencrypt/live/sewersender.se/fullchain.pem"
+# set :ssl_key,         "/etc/letsencrypt/live/sewersender.se/privkey.pem"
+
+# set :linked_files, %w{config/master.key}
+
+set :pty,             true
+# set :use_sudo,        false
+set :stage,           :production
+# set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/code/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+# set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+
+namespace :puma do
+  desc "Create Directories for Puma Pids and Socket"
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit # rubocop:disable Rails/Exit
+      end
+    end
+  end
+
+  desc "Initial Deploy"
+  task :initial do
+    on roles(:app) do
+      before "deploy:restart", "puma:start"
+      invoke "deploy"
+    end
+  end
+
+  desc "Restart application"
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke "puma:restart"
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :restart
+end
