af7e27efab9561a12131e479fbd22f6df264b3a6
zeddan <robin.saaf@gmail.com>
2021-04-19 22:06:20 +0200

    Add artists controller/views

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+class ArtistsController < ApplicationController
+  before_action :set_artist, only: %i[edit update destroy]
+
+  def index
+    @artists = Artist.all
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def edit; end
+
+  def create
+    @artist = Artist.new(artist_params)
+
+    respond_to do |format|
+      if @artist.save
+        format.html { redirect_to artists_url, notice: 'Artist was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @artist.update(artist_params)
+        format.html { redirect_to @artist, notice: 'Artist was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @artist.destroy
+    respond_to do |format|
+      format.html { redirect_to artists_url, notice: 'Artist was successfully destroyed.' }
+    end
+  end
+
+  private
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+
+  def artist_params
+    params.require(:artist).permit(:name, :artist_id)
+  end
+end
