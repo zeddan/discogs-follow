221700fc2fc9a5be152d2b53b819b4a4fd2bcae7
zeddan <robin.saaf@gmail.com>
2021-04-19 23:20:19 +0200

    :)

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,9 +19,9 @@ set :pg_generate_random_password, true
 set :linked_files, %w{config/master.key}
 
 set :pty,             true
-# set :use_sudo,        false
+set :use_sudo,        false
 set :stage,           :production
-# set :deploy_via,      :remote_cache
+set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/code/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
@@ -30,7 +30,7 @@ set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
-# set :puma_worker_timeout, nil
+set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 
 namespace :puma do
