00bbed75bb4d29e7f2d5ca661c0a4af71f9cca40
zeddan <robin.saaf@gmail.com>
2021-05-09 01:08:27 +0200

    Fix dependent destroy for followables

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,5 @@
 class Artist < ApplicationRecord
-  has_many :follows, dependent: :destroy
+  has_many :follows, dependent: :destroy, as: :followable
   has_many :releases, dependent: :destroy
   validates :discogs_artist_id, uniqueness: true
 end
